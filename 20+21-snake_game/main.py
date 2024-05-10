204973991c4f41ca9173605aa2714fc63a8bfb3c
DanyCode <danyweismail@gmail.com>
2024-05-10 08:14:48 +0200

    finished the snake game

--- 20+21-snake_game/main.py
+++ 20+21-snake_game/main.py
@@ -30,7 +30,18 @@ while game_on:
     # when snake collides with the food
     if snake.head.distance(food) < 15:
         food.refresh()
-        scoreboard.score += 1
-        scoreboard.reprint()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # detect collision
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
+
+    # detect collision with tail
+    for segment in snake.segments[1:-1]:
+        if snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
--- 20+21-snake_game/scoreboard.py
+++ 20+21-snake_game/scoreboard.py
@@ -1,23 +1,33 @@
 from turtle import Turtle
 
-TEXT_CORD = 280
+TEXT_CORD = 270
+ALIGNMENT = "center"
+FONT = ("Courier", 18, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.color("white")
-        self.speed("fastest")
-        self.penup()
-        self.goto(y=TEXT_CORD, x=0)
-        self.hideturtle()
         self.score = 0
-        self.reprint()
+        self.color("white")
+        # self.speed("fastest")
+        self.penup()
+        self.goto(0, TEXT_CORD)
+        self.hideturtle()
+        # self.reprint()
+        self.update_scoreboard()
 
-    def reprint(self):
+    def update_scoreboard(self):
+        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
         self.clear()
-        self.goto(y=TEXT_CORD, x=0)
+        self.score += 1
+        self.update_scoreboard()
 
-        self.write(arg=f"Score: {self.score}", move=True, align="center", font=('Arial', 10, 'normal'))
 
 
 
